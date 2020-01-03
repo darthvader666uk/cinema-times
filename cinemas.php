3a9be1008964573eae0f1a42fb09119ed3039c76
stu <snightingale@updraftplus.com>
2020-01-03 15:29:11 +0000

    move around

--- cinemas.php
+++ cinemas.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * darthvader666uk/cinema-times
+ * 
+ * https://www.cineworld.co.uk/uk/data-api-service/v1/10108/trailers/byCinemaId/8031?_=1578064777652
+ * https://www.cineworld.co.uk/uk/data-api-service/v1/quickbook/10108/film-events/in-cinema/8031/at-date/2020-01-03?attr=&lang=en_GB
+ * https://www.cineworld.co.uk/uk/data-api-service/v1/quickbook/10108/attributes?jsonp&lang=en_GB
+ */
+
 if(in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])){
     define('ROOT', $_SERVER['DOCUMENT_ROOT']."/");
 } else {
