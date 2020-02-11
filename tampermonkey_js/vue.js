144c48ffcac97ebda03f0442ccb13070ba8df76f
Stuart Nightingale <snightingale@updraftplus.com>
2020-02-11 12:51:55 +0000

    added vue

---
+++ tampermonkey_js/vue.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         Vue Calculate End Time
+// @version      0.1
+// @description  Trying to Calculate the end times of films to see when the film ends
+// @author       snightingale
+// @match        *://www.myvue.com/cinema/*/whats-on
+// @require      http://code.jquery.com/jquery-3.4.1.min.js
+// @run-at       document-idle
+// ==/UserScript==
+
+var $ = window.jQuery;
+
+$(document).ready(function(){
+    $('div.filmlist__item').each(function() {
+
+        console.log("Hello 1");
+
+        // Get the Length of the film
+        var filmLengthFind = $(this).find('div.filmlist__meta-container dd:first').text();
+
+        console.log("filmLengthFind:"+filmLengthFind);
+
+        // var filmLength = filmLengthFind.match(/\d+/);
+
+        // // Make sure something is returnd
+        // if (filmLength) {
+        //     console.log("filmLength:"+filmLength);
+
+        //     // Convert to Int
+        //     var filmLengthInt = parseInt(filmLength); //Output will be 234.
+    
+        //     // Get each time
+        //     $(this).find('.filmTimes a').each(function() {
+        //         // Get the Length of the film
+        //         var filmtimes = $(this).text();
+
+        //         // Show correct time with add mins
+        //         var newTime = addMinutes(filmtimes, filmLengthInt);
+    
+        //         $(this).text(filmtimes+" - "+newTime);
+    
+        //     });
+        // }
+    });
+});
+
+function addMinutes(time, minsToAdd) {
+  function D(J){ return (J<10? '0':'') + J;};
+  var piece = time.split(':');
+  var mins = piece[0]*60 + +piece[1] + +minsToAdd + 30;
+
+  return D(mins%(24*60)/60 | 0) + ':' + D(mins%60);
+}
